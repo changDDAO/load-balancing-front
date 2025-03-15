a3fa8c6d2fa7920cfd5c09f2379d551331c77fec
changDDAO <younch8342@naver.com>
2025-03-15 16:02:02 +0900

    feat: React를 사용하여 화면 개발전 api Test (axios 설치필요)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './App.css'
 import Base from "./component/Base.jsx";
+import axios from "./api/axios.js";
+import requests from "./api/request.js";
 
 function App() {
+    const fetchUrl = requests.fetchMembers;
+
+    useEffect(() => {
+        const data = async() => {
+            await axios.get(fetchUrl);
+        }
+        console.log(data);
+    }, []);
 
 
   return (
