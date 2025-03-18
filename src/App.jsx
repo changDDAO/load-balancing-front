971f78f7b1abeef329491faf2dcc5937770d9349
changDDAO <younch8342@naver.com>
2025-03-18 22:57:34 +0900

    feat: axios로 delete 및 put mapping 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import axios from "./api/axios.js";
 import memberRequests from "./api/request.js";
 
 function App() {
-    const fetchUrl = memberRequests.fetchMembers;
+    const id = 2;
+    const fetchUrl = memberRequests.update(id);
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = axios.get(fetchUrl);
+            const response = axios.put(fetchUrl,{name:"changed"});
             console.log("response : ", response);
         }
         fetchData();
